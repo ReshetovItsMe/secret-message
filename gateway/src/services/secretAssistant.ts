06c6f17682d7366914df6935c5939a04ddc3b05f
Vladislav Reshetov <reshetovitsme@gmail.com>
2023-05-17 10:11:05 +0300

    fix docker problems

--- gateway/src/services/secretAssistant.ts
+++ gateway/src/services/secretAssistant.ts
@@ -1,9 +1,9 @@
-import grpc from '@grpc/grpc-js';
+import { credentials } from '@grpc/grpc-js';
 import { SecretAssistantClient } from '../../proto/message_grpc_pb';
 
 const client = new SecretAssistantClient(
-    process.env.SECRET_ASSISTANT_URL ?? 'localhost:50051',
-    grpc.credentials.createInsecure()
+    process.env.SECRET_ASSISTANT_URL ?? 'secret-assistant:50052',
+    credentials.createInsecure()
 );
 
 export default client;
