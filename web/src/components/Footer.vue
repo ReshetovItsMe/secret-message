5d713090bf4c61aa48d84f6b553c8b324f1c22f9
Vladislav Reshetov <reshetovitsme@gmail.com>
2023-10-11 16:04:57 +0300

    chore: project structure

---
+++ web/src/components/Footer.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+    <h1>Footer</h1>
+</template>
---
+++ web/src/components/icons/secretMessage/SecretMessageLogo.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import SecretMessageLogo from './logo.png';
+</script>
+
+<template>
+    <SecretMessageLogo />
+</template>
