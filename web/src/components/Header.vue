31000eb0c4269fff8eee2bbff94dea1cc1308c1b
Vladislav Reshetov <reshetovitsme@gmail.com>
2024-02-02 16:18:19 +0300

    feat: navigation for header

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import SecretMessageLogo from './icons/secretMessage/SecretMessageLogo.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const onClickLogo = () => {
+    router.push('/');
+};
 </script>
 
 <template>
     <div class="header">
-        <SecretMessageLogo />
+        <SecretMessageLogo class="logo" @click="onClickLogo" />
     </div>
 </template>
 
@@ -13,5 +20,8 @@ import SecretMessageLogo from './icons/secretMessage/SecretMessageLogo.vue';
     display: flex;
     justify-content: flex-start;
     height: 100%;
+}
+.logo {
+    cursor: pointer;
 }
 </style>
